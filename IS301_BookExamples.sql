cb77fcde0e67c6b16517b35adca50249a0f8f98e
Joseph Phillips <josephephillips27@gmail.com>
2011-11-12 11:34:23 -0800

    SELECT QUERIES with comparisions

--- IS301_BookExamples.sql
+++ IS301_BookExamples.sql
@@ -136,8 +136,8 @@ WHERE P_MIN = 5
 	Loading data rows					*/
 
 /* VENDOR rows						*/
-INSERT INTO VENDOR VALUES(21225,'Bryson, Inc.'    ,'Smithson','615','223-3234','TN','Y');
-INSERT INTO VENDOR VALUES(21226,'SuperLoo, Inc.'  ,'Flushing','904','215-8995','FL','N');
+-- ADDED ABOVE INSERT INTO VENDOR VALUES(21225,'Bryson, Inc.'    ,'Smithson','615','223-3234','TN','Y');
+-- ADDED ABOVE INSERT INTO VENDOR VALUES(21226,'SuperLoo, Inc.'  ,'Flushing','904','215-8995','FL','N');
 INSERT INTO VENDOR VALUES(21231,'D&E Supply'     ,'Singh'   ,'615','228-3245','TN','Y');
 INSERT INTO VENDOR VALUES(21344,'Gomez Bros.'     ,'Ortega'  ,'615','889-2546','KY','N');
 INSERT INTO VENDOR VALUES(22567,'Dome Supply'     ,'Smith'   ,'901','678-1419','GA','N');
@@ -149,8 +149,8 @@ INSERT INTO VENDOR VALUES(25501,'Damal Supplies'  ,'Smythe'  ,'615','890-3529','
 INSERT INTO VENDOR VALUES(25595,'Rubicon Systems' ,'Orton'   ,'904','456-0092','FL','Y');
 
 /*PRODUCT rows						*/
-INSERT INTO PRODUCT VALUES('11QER/31','Power painter, 15 psi., 3-nozzle'     ,'03-NOV-2009',  8,  5,109.99,0.00,25595);
-INSERT INTO PRODUCT VALUES('13-Q2/P2','7.25-in. pwr. saw blade'              ,'13-DEC-2009', 32, 15, 14.99,0.05,21344);
+-- ADDED ABOVE INSERT INTO PRODUCT VALUES('11QER/31','Power painter, 15 psi., 3-nozzle'     ,'03-NOV-2009',  8,  5,109.99,0.00,25595);
+-- ADDED ABOVE INSERT INTO PRODUCT VALUES('13-Q2/P2','7.25-in. pwr. saw blade'              ,'13-DEC-2009', 32, 15, 14.99,0.05,21344);
 INSERT INTO PRODUCT VALUES('14-Q1/L3','9.00-in. pwr. saw blade'              ,'13-NOV-2009', 18, 12, 17.49,0.00,21344);
 INSERT INTO PRODUCT VALUES('1546-QQ2','Hrd. cloth, 1/4-in., 2x50'            ,'15-JAN-2010', 15,  8, 39.95,0.00,23119);
 INSERT INTO PRODUCT VALUES('1558-QW1','Hrd. cloth, 1/2-in., 3x50'            ,'15-JAN-2010', 23,  5, 43.99,0.00,23119);
@@ -166,7 +166,28 @@ INSERT INTO PRODUCT VALUES('SM-18277','1.25-in. metal screw, 25'             ,'0
 INSERT INTO PRODUCT VALUES('SW-23116','2.5-in. wd. screw, 50'                ,'24-FEB-2010',237,100,  8.45,0.00,21231);
 INSERT INTO PRODUCT VALUES('WR3/TT3' ,'Steel matting, 4''x8''x1/6", .5" mesh','17-JAN-2010', 18,  5,119.95,0.10,25595);
 
+-- Find equal to number
 Select P_DESCRIPT, P_INDATE, P_PRICE, V_CODE
 From PRODUCT
 WHERE V_CODE = 21344
+
+-- Find not equal to number
+SELECT P_DESCRIPT, P_INDATE, P_PRICE, V_CODE
+FROM PRODUCT
+WHERE V_CODE <> 21344
+
+-- Find less than or equal to number
+SELECT P_DESCRIPT, P_QOH, P_MIN, P_PRICE
+FROM PRODUCT
+WHERE P_PRICE <=10
+
+--Find string that is less than String A,B,C  Select * FROM Table WHERE variable < B -- Result A
+SELECT P_CODE, P_DESCRIPT, P_QOH, P_MIN, P_PRICE
+FROM PRODUCT
+WHERE P_CODE < '1558-QW1'
+
+-- Find date that is compared to variable
+SELECT P_DESCRIPT, P_QOH, P_MIN, P_PRICE, P_INDATE
+FROM PRODUCT
+WHERE P_INDATE >= '20-JAN-2010'
 
