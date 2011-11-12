9eceecf3feeaa82d96704209e6c924a5afe863db
Joseph Phillips <josephephillips27@gmail.com>
2011-11-12 11:23:35 -0800

    Added data and a SELECT Query with restrictions

--- IS301_BookExamples.sql
+++ IS301_BookExamples.sql
@@ -84,7 +84,6 @@ VALUES (21225,'Bryson, Inc.','Smithson','615','223-3234','TN','Y')
 INSERT INTO VENDOR
 VALUES (21226,'Superloo, Inc.','Flushing','904','215-8995','FL','N')
 
-SELECT * FROM VENDOR
 
 INSERT INTO PRODUCT
 VALUES ('11QER/31','Power painter, 15 psi., 3-nozzle','11-03-09',8,5,109.99,0.00,21225)
@@ -103,8 +102,6 @@ INSERT INTO PRODUCT	(P_CODE , P_DESCRIPT) VALUES (	'BTR-345','Titanium drill bit
 -- Simply put saves all changes.
 COMMIT
 
--- SELECT QUERIES 
-SELECT * FROM PRODUCT
 
 -- Trick to using the intelisense is to fill out the FROM [TABLE] first
 SELECT P_CODE, P_DESCRIPT, P_QOH, P_MIN, P_PRICE,P_DISCOUNT,V_CODE
@@ -116,13 +113,60 @@ UPDATE PRODUCT
 SET		P_INDATE ='18-JAN-2010'
 WHERE	P_CODE = '13-Q2/P2'
 
--- CHECK 
-SELECT * FROM PRODUCT
 
 -- REPLACE THREE ITEMS WHERE P_CODE = 13-Q2/P2
 UPDATE PRODUCT
 SET		P_INDATE ='18-JAN-2010',P_PRICE = 17.99, P_MIN = 10
 WHERE	P_CODE = '13-Q2/P2'
 
--- CHECK 
-SELECT * FROM PRODUCT
+
+-- DELETE Queries
+/* Removing to keep data for next section
+DELETE FROM PRODUCT
+WHERE P_CODE = 'BTR-345'
+
+DELETE FROM PRODUCT
+WHERE P_MIN = 5
+
+*/
+
+/*
+    SELECT QUERIES with conditional restricitions
+	Adding data to help with this section
+	Loading data rows					*/
+
+/* VENDOR rows						*/
+INSERT INTO VENDOR VALUES(21225,'Bryson, Inc.'    ,'Smithson','615','223-3234','TN','Y');
+INSERT INTO VENDOR VALUES(21226,'SuperLoo, Inc.'  ,'Flushing','904','215-8995','FL','N');
+INSERT INTO VENDOR VALUES(21231,'D&E Supply'     ,'Singh'   ,'615','228-3245','TN','Y');
+INSERT INTO VENDOR VALUES(21344,'Gomez Bros.'     ,'Ortega'  ,'615','889-2546','KY','N');
+INSERT INTO VENDOR VALUES(22567,'Dome Supply'     ,'Smith'   ,'901','678-1419','GA','N');
+INSERT INTO VENDOR VALUES(23119,'Randsets Ltd.'   ,'Anderson','901','678-3998','GA','Y');
+INSERT INTO VENDOR VALUES(24004,'Brackman Bros.'  ,'Browning','615','228-1410','TN','N');
+INSERT INTO VENDOR VALUES(24288,'ORDVA, Inc.'     ,'Hakford' ,'615','898-1234','TN','Y');
+INSERT INTO VENDOR VALUES(25443,'B&K, Inc.'      ,'Smith'   ,'904','227-0093','FL','N');
+INSERT INTO VENDOR VALUES(25501,'Damal Supplies'  ,'Smythe'  ,'615','890-3529','TN','N');
+INSERT INTO VENDOR VALUES(25595,'Rubicon Systems' ,'Orton'   ,'904','456-0092','FL','Y');
+
+/*PRODUCT rows						*/
+INSERT INTO PRODUCT VALUES('11QER/31','Power painter, 15 psi., 3-nozzle'     ,'03-NOV-2009',  8,  5,109.99,0.00,25595);
+INSERT INTO PRODUCT VALUES('13-Q2/P2','7.25-in. pwr. saw blade'              ,'13-DEC-2009', 32, 15, 14.99,0.05,21344);
+INSERT INTO PRODUCT VALUES('14-Q1/L3','9.00-in. pwr. saw blade'              ,'13-NOV-2009', 18, 12, 17.49,0.00,21344);
+INSERT INTO PRODUCT VALUES('1546-QQ2','Hrd. cloth, 1/4-in., 2x50'            ,'15-JAN-2010', 15,  8, 39.95,0.00,23119);
+INSERT INTO PRODUCT VALUES('1558-QW1','Hrd. cloth, 1/2-in., 3x50'            ,'15-JAN-2010', 23,  5, 43.99,0.00,23119);
+INSERT INTO PRODUCT VALUES('2232/QTY','B&D jigsaw, 12-in. blade'            ,'30-DEC-2009',  8,  5,109.92,0.05,24288);
+INSERT INTO PRODUCT VALUES('2232/QWE','B&D jigsaw, 8-in. blade'             ,'24-DEC-2009',  6,  5, 99.87,0.05,24288);
+INSERT INTO PRODUCT VALUES('2238/QPD','B&D cordless drill, 1/2-in.'         ,'20-JAN-2010', 12,  5, 38.95,0.05,25595);
+INSERT INTO PRODUCT VALUES('23109-HB','Claw hammer'                          ,'20-JAN-2010', 23, 10,  9.95,0.10,21225);
+INSERT INTO PRODUCT VALUES('23114-AA','Sledge hammer, 12 lb.'                ,'02-JAN-2010',  8,  5, 14.40,0.05,NULL);
+INSERT INTO PRODUCT VALUES('54778-2T','Rat-tail file, 1/8-in. fine'          ,'15-DEC-2009', 43, 20,  4.99,0.00,21344);
+INSERT INTO PRODUCT VALUES('89-WRE-Q','Hicut chain saw, 16 in.'              ,'07-FEB-2010', 11,  5,256.99,0.05,24288);
+INSERT INTO PRODUCT VALUES('PVC23DRT','PVC pipe, 3.5-in., 8-ft'              ,'20-FEB-2010',188, 75,  5.87,0.00,NULL);
+INSERT INTO PRODUCT VALUES('SM-18277','1.25-in. metal screw, 25'             ,'01-MAR-2010',172, 75,  6.99,0.00,21225);
+INSERT INTO PRODUCT VALUES('SW-23116','2.5-in. wd. screw, 50'                ,'24-FEB-2010',237,100,  8.45,0.00,21231);
+INSERT INTO PRODUCT VALUES('WR3/TT3' ,'Steel matting, 4''x8''x1/6", .5" mesh','17-JAN-2010', 18,  5,119.95,0.10,25595);
+
+Select P_DESCRIPT, P_INDATE, P_PRICE, V_CODE
+From PRODUCT
+WHERE V_CODE = 21344
+
